c3704a3181a1d24c9c4d317c65087ed6d68ff71a
danieldema <danieldema42@gmail.com>
2025-04-29 22:28:49 -0400

    completed income analysis

---
+++ aerofit_analysis/aerofit_analysis_income.sql
@@ -0,0 +1,53 @@
+-- In this part of the project we analyze product preference by income. The mean income is $53720 and median income is $50596. The product with customers with the highest mean and
+-- median income is KP781 at mean $75442 and median $76568, followed by KP481 with mean $48974 and median $49460, followed by KP281 with mean $46418 and median $46617.
+
+SELECT MIN(Income)
+FROM Aerofit;
+
+-- min
+-- 29562
+
+SELECT MIN(Income)
+FROM Aerofit;
+
+-- max
+-- 104581
+
+SELECT ROUND(AVG(Income)) AS mean,
+ROUND(PERCENTILE_CONT(0.5)
+WITHIN GROUP (Order By Income))
+AS median
+FROM Aerofit;
+
+--"mean"	    "median"
+-- 53720	     50596
+
+SELECT ROUND(AVG(Income)) AS mean,
+ROUND(PERCENTILE_CONT(0.5)
+WITHIN GROUP (Order By Income))
+AS median
+FROM Aerofit
+WHERE Product = 'KP281';
+
+--"mean"	    "median"
+-- 46418	     46617
+
+SELECT ROUND(AVG(Income)) AS mean,
+ROUND(PERCENTILE_CONT(0.5)
+WITHIN GROUP (Order By Income))
+AS median
+FROM Aerofit
+WHERE Product = 'KP481';
+
+--"mean"	    "median"
+-- 48974	     49460
+
+SELECT ROUND(AVG(Income)) AS mean,
+ROUND(PERCENTILE_CONT(0.5)
+WITHIN GROUP (Order By Income))
+AS median
+FROM Aerofit
+WHERE Product = 'KP781';
+
+--"mean"	    "median"
+-- 75442	     76568
